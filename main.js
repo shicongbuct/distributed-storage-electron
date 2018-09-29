bb89e53fb1cc9249e6bbc795b6bfdaf34c362ff8
shicongbuct <shicongbuct@126.cn>
2018-09-29 17:47:08 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const electron = require('electron');
+
+const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
+
+const debug = /--debug/.test(process.argv[2])
+
+let mainWindow = null;
+function initialize() {
+    let shouldQuit = makeSingleInstance();
+    if (shouldQuit) return app.quit();
+
+    function createWindow () {
+        var windowOptions = {
+            width: 1080,
+            minWidth: 680,
+            height: 840,
+            title: app.getName()
+        }
+
+        // if (process.platform === 'linux') {
+        //     windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+        // }
+
+        mainWindow = new BrowserWindow(windowOptions);
+        mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
+
+        // Launch fullscreen with DevTools open, usage: npm run debug
+        if (debug) {
+            mainWindow.webContents.openDevTools();
+            mainWindow.maximize();
+            require('devtron').install();
+        }
+
+        mainWindow.on('closed', function () {
+            mainWindow = null
+        })
+    }
+
+    app.on('ready', function () {
+        createWindow();
+        // autoUpdater.initialize();
+    });
+
+    app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    })
+
+    app.on('activate', function () {
+        if (mainWindow === null) {
+            createWindow()
+        }
+    })
+}
+
+function makeSingleInstance () {
+    if (process.mas) return false;
+
+    return app.makeSingleInstance(function () {
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore();
+            mainWindow.focus();
+        }
+    })
+}
+
+initialize();
+
+
+
